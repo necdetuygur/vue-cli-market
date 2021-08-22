6a53f3e1f3bd938a15e21e29e0b7d5c94b350c04
Necdet UYGUR <necdet.uygur@gmail.com>
2021-08-22 21:00:07 +0300

    margin

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
-    <div class="col-md-6 mt-3"><Add /></div>
-    <div class="col-md-6 mt-3"><List /></div>
+    <div class="col-md-6 mt-3 mb-3"><Add /></div>
+    <div class="col-md-6 mt-3 mb-3"><List /></div>
   </div>
 </template>
 <script>
