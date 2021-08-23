f9600c60c767d5727b69e9356047ab9a6e0d460e
Necdet UYGUR <necdet.uygur@gmail.com>
2021-08-23 13:49:59 +0300

    New Deployment

---
+++ precache-manifest.f5cf0cdb63cf7e39eebd6dc2b7699c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05613dbdd39ca120094fbe025437768d",
+    "url": "https://necdetuygur.github.io/vue-cli-market/css/bootstrap.min.css"
+  },
+  {
+    "revision": "93fa38cad56c64936e44b54a87a3ef5e",
+    "url": "https://necdetuygur.github.io/vue-cli-market/index.html"
+  },
+  {
+    "revision": "4eacd34299ed668824da",
+    "url": "https://necdetuygur.github.io/vue-cli-market/js/app.c57d0c06.js"
+  },
+  {
+    "revision": "1a3586efb78adde29293",
+    "url": "https://necdetuygur.github.io/vue-cli-market/js/chunk-vendors.47e68896.js"
+  },
+  {
+    "revision": "cd14c23ab90fc3dec84817fc76a26a10",
+    "url": "https://necdetuygur.github.io/vue-cli-market/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "https://necdetuygur.github.io/vue-cli-market/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://necdetuygur.github.io/vue-cli-market/precache-manifest.f5cf0cdb63cf7e39eebd6dc2b7699c1b.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-cli-market"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
